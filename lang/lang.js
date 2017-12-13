f50b5f55693fe81a42a20ae47da1ec4e385293b6
budak <thatbudakguy@gmail.com>
2017-12-12 16:27:58 -0800

    wip rxjs

--- lang/lang.js
+++ lang/lang.js
@@ -1,72 +1,114 @@
 /* eslint-disable max-len */
 
-let palette1 = [
-  '#ff6384',
-  '#ffce56',
-  '#36a2eb',
-  '#4f7942',
-]
-
-let palette2 = [
-    '#2E2D4D',
-    '#337357',
-    '#6D9F71',
-    '#E4E3D3',
-]
-
-let colorMap = palette1
-
-let languagesChart = new Chart($('#winthrop'), {
+let options = {
   type: 'pie',
-  data: {
-    labels: ['English', 'Latin', 'Greek', 'Symbol'],
-    datasets: [{
-        label: 'Princeps (selections)',
-        data: [0, 3, 0, 48],
-        backgroundColor: colorMap,
-      },
-      {
-        label: 'The Triall of Witch-craft, shewing the True and Right Methode of the Discovery, with A Confutation of erroneous wayes',
-        data: [14, 8, 0, 18],
-        backgroundColor: colorMap,
-      },
-      {
-        label: 'Chronologia sacra (selections)',
-        data: [0, 1, 0, 18],
-        backgroundColor: colorMap,
-      },
-      {
-        label: '[...] Flores: ex operibus [...] singulari iudicio selecti',
-        data: [4, 3, 1, 9],
-        backgroundColor: colorMap,
-      },
-      {
-        label: 'De republica Anglorum. The maner of Governement or policie of the Realme of England',
-        data: [9, 4, 0, 3],
-        backgroundColor: colorMap,
-      },
+  labels: [
+    'English',
+    'Latin',
+    'Greek',
+    'Symbol',
+  ],
+  palettes: {
+    a: [
+      '#2E2D4D',
+      '#337357',
+      '#6D9F71',
+      '#E4E3D3',
+    ],
+    b: [
+      '#ff6384',
+      '#ffce56',
+      '#36a2eb',
+      '#4f7942',
     ],
   },
-  options: {
-    legend: {
-      display: true,
-    },
-  },
-})
+}
 
-let booksList = languagesChart.data.datasets.map((ds) => {
-    return `<li class="list-group-item"><i>${ds.label}</i></li>`
-}).join('')
+let type = Rx.Observable.of(options.type)
+let colors = Rx.Observable.of(options.palettes.a)
+let labels = Rx.Observable.of(options.labels)
 
-$('.books-list').html(booksList)
+let json = Rx.Observable.fromPromise($.getJSON('languages.json'))
+// let books = datasets.flatMap(Rx.Observable.from)
+let datasets = Rx.Observable.combineLatest(json, colors,
+    (json, colors) => json.map((book) => {
+        return {
+            label: book.label,
+            data: book.data,
+            backgroundColor: colors
+        }
+    })
+)
 
-$('.non-na').click((e) => {
-  languagesChart.data.datasets = languagesChart.data.datasets.map((ds) => {
+let chart = Rx.Observable.combineLatest(type, datasets, labels,
+    (type, datasets, labels) => {
     return {
-      label: ds.label,
-      data: ds.data.slice(0, 3),
-      backgroundColor: ds.backgroundColor,
+        type: type,
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: {
+            legend: {
+                display: true
+            }
+        }
     }
-  })
-  languagesChart.update()
 })
+
+chart.subscribe((chart) => new Chart($('#winthrop'), chart))
+
+// let languagesChart = new Chart($('#winthrop'), {
+//   type: 'pie',
+//   data: {
+//     labels: ['English', 'Latin', 'Greek', 'Symbol'],
+//     datasets: [{
+//         label: 'Princeps (selections)',
+//         data: [0, 3, 0, 48],
+//         backgroundColor: colorMap,
+//       },
+//       {
+//         label: 'The Triall of Witch-craft, shewing the True and Right Methode of the Discovery, with A Confutation of erroneous wayes',
+//         data: [14, 8, 0, 18],
+//         backgroundColor: colorMap,
+//       },
+//       {
+//         label: 'Chronologia sacra (selections)',
+//         data: [0, 1, 0, 18],
+//         backgroundColor: colorMap,
+//       },
+//       {
+//         label: '[...] Flores: ex operibus [...] singulari iudicio selecti',
+//         data: [4, 3, 1, 9],
+//         backgroundColor: colorMap,
+//       },
+//       {
+//         label: 'De republica Anglorum. The maner of Governement or policie of the Realme of England',
+//         data: [9, 4, 0, 3],
+//         backgroundColor: colorMap,
+//       },
+//     ],
+//   },
+//   options: {
+//     legend: {
+//       display: true,
+//     },
+//   },
+// })
+//
+// let booksList = languagesChart.data.datasets.map((ds) => {
+//     return `<li class="list-group-item"><i>${ds.label}</i></li>`
+// }).join('')
+//
+// $('.books-list').html(booksList)
+//
+// $('.non-na').click((e) => {
+//   languagesChart.data.datasets = languagesChart.data.datasets.map((ds) => {
+//     return {
+//       label: ds.label,
+//       data: ds.data.slice(0, 3),
+//       backgroundColor: ds.backgroundColor,
+//     }
+//   })
+//   languagesChart.update()
+// })
